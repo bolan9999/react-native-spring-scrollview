79e11bfe4a8d910e097148fd340705e07b0078f4
bolan9999 <shanshang130@gmail.com>
2021-07-12 16:05:06 +0800

    安卓调试完成

--- android/app/src/main/java/com/ssvexample/MainApplication.java
+++ android/app/src/main/java/com/ssvexample/MainApplication.java
@@ -2,6 +2,9 @@ package com.ssvexample;
 
 import android.app.Application;
 import android.content.Context;
+
+import com.airbnb.android.react.lottie.LottiePackage;
+import com.bolan9999.SpringScrollViewPackage;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactInstanceManager;
@@ -24,8 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
+            packages.add(new SpringScrollViewPackage());
+            packages.add(new LottiePackage());
           return packages;
         }
 
